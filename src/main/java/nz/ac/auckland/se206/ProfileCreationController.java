3294f16a78522f5f120ee85a583e75a41cd58598
jason <kper068@aucklanduni.ac.nz>
2022-09-13 17:06:32 +1200

    feat - Design the basic interface for the profile creation scene
    Created the Controller class and the corresponding fxml file for the
    profile creation scene.
    Implemented username and password text fields.
    Implemented buttons to confirm creation of new profile or cancel.

---
+++ src/main/java/nz/ac/auckland/se206/ProfileCreationController.java
@@ -0,0 +1,31 @@
+package nz.ac.auckland.se206;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ProfileCreationController {
+
+	@FXML
+	private TextField usernameTextField;
+
+	@FXML
+	private PasswordField passwordTextField;
+
+	@FXML
+	private Button confirmButton;
+
+	@FXML
+	private Button cancelButton;
+
+	@FXML
+	private void onConfirm() {
+
+	}
+
+	@FXML
+	private void onCancel() {
+
+	}
+}
--- src/main/java/nz/ac/auckland/se206/SceneManager.java
+++ src/main/java/nz/ac/auckland/se206/SceneManager.java
@@ -1,22 +1,22 @@
 package nz.ac.auckland.se206;
 
 import java.util.HashMap;
+
 import javafx.scene.Parent;
 
 public class SceneManager {
 
-  public enum AppUi {
-    MAIN_MENU,
-    CANVAS
-  }
+	public enum AppUi {
+		MAIN_MENU, CANVAS, PROFILE_CREATION
+	}
 
-  private static HashMap<AppUi, Parent> sceneMap = new HashMap<AppUi, Parent>();
+	private static HashMap<AppUi, Parent> sceneMap = new HashMap<AppUi, Parent>();
 
-  public static void addUi(AppUi appUi, Parent uiRoot) {
-    sceneMap.put(appUi, uiRoot);
-  }
+	public static void addUi(AppUi appUi, Parent uiRoot) {
+		sceneMap.put(appUi, uiRoot);
+	}
 
-  public static Parent getUiRoot(AppUi appUi) {
-    return sceneMap.get(appUi);
-  }
+	public static Parent getUiRoot(AppUi appUi) {
+		return sceneMap.get(appUi);
+	}
 }

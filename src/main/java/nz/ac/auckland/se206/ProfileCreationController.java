faa81de8db498971b3ccbe2dedfbf517e258fca8
jason <kper068@aucklanduni.ac.nz>
2022-09-13 20:32:13 +1200

    feat - Implement a way for new profiles to be stored locally
    Implemented the functionality of the confirm button.
    Store newly created profiles into a text file database.

--- src/main/java/nz/ac/auckland/se206/ProfileCreationController.java
+++ src/main/java/nz/ac/auckland/se206/ProfileCreationController.java
@@ -1,5 +1,8 @@
 package nz.ac.auckland.se206;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -21,7 +24,19 @@ public class ProfileCreationController {
 
 	@FXML
 	private void onConfirm() {
+		// User Profile Save Format (Case Sensitive)
+		// username|password|games_won|games_lost|words_in_previous_runs|avg_time|fastest_win
+		String username = usernameTextField.getText();
+		String password = passwordTextField.getText();
 
+		// Appending new user profile to the database with all profiles
+		try {
+			FileWriter writer = new FileWriter("/resources/profiles.txt");
+			writer.append(username + "|" + password + "|" + 0 + "|" + 0 + "|" + "" + "|" + 0 + "|" + 0);
+			writer.close();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 	}
 
 	@FXML

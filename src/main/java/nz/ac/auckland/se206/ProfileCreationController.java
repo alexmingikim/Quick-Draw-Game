6a961d0baa6499393c5b6e48b4be392e88cdfe56
Alex Kim <mkim897@aucklanduni.ac,nz>
2022-09-20 18:37:52 +1200

    feat - Allow multiple user profiles to be stored in single JSON file

--- src/main/java/nz/ac/auckland/se206/ProfileCreationController.java
+++ src/main/java/nz/ac/auckland/se206/ProfileCreationController.java
@@ -2,12 +2,17 @@ package nz.ac.auckland.se206;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
@@ -33,7 +38,7 @@ public class ProfileCreationController {
 
   @FXML
   private void onConfirm(ActionEvent event) throws IOException {
-    // Generate error if username field or password field are blank
+    // generate error if username field is blank
     if (usernameTextField.getText().isBlank()) {
       Alert alert = new Alert(AlertType.ERROR);
       alert.setTitle("Empty username");
@@ -52,20 +57,29 @@ public class ProfileCreationController {
 
     }
 
-    // User Profile Save Format (Case Sensitive)
-    // username|password|games_won|games_lost|words_in_previous_runs|avg_time|fastest_win
+    // keep track of username and user id
     String username = usernameTextField.getText();
-
-    // keep track of user id
     String userId = ProfileViewController.getCurrentUserId();
 
     usernameTextField.clear();
 
     // create new JSON file
-    User user = new User(userId, username, "0", "0", "0", "-", "0", "-", "");
-    FileWriter writer = new FileWriter("profiles/profiles.json");
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    gson.toJson(user, writer);
+
+    List<User> userProfiles = new ArrayList<User>();
+    try {
+      // read existing user profiles from JSON file and store into array list
+      FileReader fr = new FileReader("profiles/profiles.json");
+      userProfiles = gson.fromJson(fr, new TypeToken<List<User>>() {}.getType());
+      fr.close();
+    } catch (FileNotFoundException e) {
+
+    }
+
+    // add new user profile to array list and store as JSON file
+    userProfiles.add(new User(userId, username, "0", "0", "0", "-", "0", "-", ""));
+    FileWriter writer = new FileWriter("profiles/profiles.json");
+    gson.toJson(userProfiles, writer);
     writer.flush();
     writer.close();
 

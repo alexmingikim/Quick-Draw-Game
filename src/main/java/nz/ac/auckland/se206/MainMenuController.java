ed8f186ce4cfb9ec0b96ddc926e1d02e953abee5
jason <kper068@aucklanduni.ac.nz>
2022-10-08 20:21:21 +1300

    fix - Fix bug where word difficulty doesn't change
    Fixed the bug where the difficulty of the words stays the same even
    after changing difficulty settings.

--- src/main/java/nz/ac/auckland/se206/MainMenuController.java
+++ src/main/java/nz/ac/auckland/se206/MainMenuController.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se206;
 
+import ai.djl.ModelException;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -35,8 +36,8 @@ public class MainMenuController {
   }
 
   @FXML
-  private void onPlay(ActionEvent event) throws IOException {
-    ((CanvasController) SceneManager.getLoader(AppUi.CANVAS).getController()).subInitialize();
+  private void onPlay(ActionEvent event) throws IOException, ModelException {
+    ((CanvasController) SceneManager.getLoader(AppUi.CANVAS).getController()).initialize();
     // set root to profile view scene if "let's start" button is pressed
     Button button = (Button) event.getSource();
     Scene sceneButtonIsIn = button.getScene();

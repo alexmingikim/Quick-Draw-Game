ac0d49e60b2204a25b237498ebcca6920ab42399
justin <justinhuang2020@gmail.com>
2022-10-16 13:22:35 +1300

    feat - Implement predictions for zen mode

--- src/main/java/nz/ac/auckland/se206/ZenModeController.java
+++ src/main/java/nz/ac/auckland/se206/ZenModeController.java
@@ -1,6 +1,10 @@
 package nz.ac.auckland.se206;
 
 import ai.djl.ModelException;
+import ai.djl.modality.Classifications;
+import ai.djl.translate.TranslateException;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -8,7 +12,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.concurrent.Task;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
@@ -19,16 +27,30 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javax.imageio.ImageIO;
 import nz.ac.auckland.se206.ml.DoodlePrediction;
 import nz.ac.auckland.se206.speech.TextToSpeech;
 
 public class ZenModeController {
+
+  static final int NUMBER_EASY_WORDS = 145;
+
+  static final int NUMBER_MEDIUM_WORDS = 132;
+
+  static final int NUMBER_HARD_WORDS = 68;
+
+  static final int TOTAL_NUMBER_WORDS = NUMBER_EASY_WORDS + NUMBER_MEDIUM_WORDS + NUMBER_HARD_WORDS;
 
   @FXML private Canvas canvas;
 
@@ -76,7 +98,15 @@ public class ZenModeController {
 
   private TextToSpeech textToSpeech = new TextToSpeech();
 
+  private Timeline timeline;
+
   private Stage stage;
+
+  private int predictionRank = 1;
+
+  private int prevPredictionRank = 0;
+
+  private boolean isStarted = false;
 
   // mouse coordinates
   private double currentX;
@@ -121,6 +151,7 @@ public class ZenModeController {
     }
 
     enablePen();
+    start();
   }
 
   /**
@@ -314,6 +345,8 @@ public class ZenModeController {
           currentX = e.getX();
           currentY = e.getY();
           blankStatus = false;
+          System.out.print("TT");
+          isStarted = true;
         });
 
     canvas.setOnMouseDragged(
@@ -335,6 +368,156 @@ public class ZenModeController {
           currentX = x;
           currentY = y;
         });
+  }
+
+  private void start() {
+
+    timeline =
+        new Timeline(
+            (new KeyFrame(
+                Duration.seconds(1),
+                e -> {
+                  try {
+                    // updating the predictions list every second
+                    updatePrediction();
+                  } catch (TranslateException e1) {
+                    e1.printStackTrace();
+                  }
+                })));
+
+    timeline.setCycleCount(Animation.INDEFINITE);
+    timeline.playFromStart();
+    KeyFrame kf =
+        new KeyFrame(
+            Duration.seconds(1),
+            event -> {
+              //		              if (counter <= 0) {
+              //		                timeline.stop();
+              //		              }
+            });
+    timeline.getKeyFrames().addAll(kf, new KeyFrame(Duration.seconds(1)));
+    timeline.play();
+  }
+
+  /**
+   * Get the current snapshot of the canvas.
+   *
+   * @return The BufferedImage corresponding to the current canvas content.
+   */
+  public BufferedImage getCurrentSnapshot() {
+    final Image snapshot = canvas.snapshot(null, null);
+    final BufferedImage image = SwingFXUtils.fromFXImage(snapshot, null);
+
+    // Convert into a binary image.
+    final BufferedImage imageBinary =
+        new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+
+    final Graphics2D graphics = imageBinary.createGraphics();
+
+    graphics.drawImage(image, 0, 0, null);
+
+    // To release memory we dispose.
+    graphics.dispose();
+
+    return imageBinary;
+  }
+
+  /**
+   * Retrieves the predictions for the current snapshot of the canvas and displays the top 10
+   * predictions as well as an indicator for whether the AI is close to guessing the current word.
+   *
+   * @throws TranslateException if the model translation has an error
+   */
+  private void updatePrediction() throws TranslateException {
+    if (isStarted == true) {
+      // get top 10 predictions
+      List<Classifications.Classification> predictions =
+          model.getPredictions(getCurrentSnapshot(), 10);
+      // get all predictions
+      List<Classifications.Classification> predictionsAll =
+          model.getPredictions(getCurrentSnapshot(), TOTAL_NUMBER_WORDS);
+      final StringBuilder sb = new StringBuilder();
+      int i = 1;
+
+      // get the rank of the prediction
+      predictionRank = 1;
+      for (Classifications.Classification prediction : predictionsAll) {
+        if (prediction.getClassName().equals(category.replace(" ", "_"))) {
+          break;
+        }
+        predictionRank++;
+      }
+
+      // set message depending on if user is getting further or closer
+      if (predictionRank > prevPredictionRank) {
+        lblStatus.setText("Getting further...");
+      } else {
+        lblStatus.setText("Getting closer!!!");
+      }
+
+      // statusLabel.setText(predictionRank + " " + prevPredictionRank);
+      prevPredictionRank = predictionRank;
+
+      // set the labels
+      predictionsTitleLabel.setText("ROBO'S PREDICTIONS");
+      predictionsLabel.setText("Top 10 Predictions\n");
+
+      // clear the text flow
+      predictionsTextFlow.getChildren().clear();
+
+      // set the font
+      Font font;
+
+      // for all predictions, print its ranking and if a prediction is in top 3 and
+      // matches with the category, call the player win method
+      for (final Classifications.Classification prediction : predictions) {
+        Text text = new Text();
+        sb.setLength(0);
+
+        // change the display format of the prediction word depending on length
+        String word = prediction.getClassName().replace("_", " ");
+
+        // set the font to be bold if the guess is the category
+        if (prediction.getClassName().equals(category.replace(" ", "_"))) {
+          font = Font.font("Courier New", FontWeight.BOLD, FontPosture.REGULAR, 16);
+          // change message depending on prediction ranking
+          if (i > 5) {
+            lblStatus.setText("Almost there!! You're doing great!");
+          } else if (i <= 5 && i != 1) {
+            lblStatus.setText("So close!!! You can do it!");
+          } else if (i == 1) {
+            lblStatus.setText("Perfect drawing! Good job!");
+          }
+        } else {
+          font = Font.font("Courier New", FontWeight.NORMAL, FontPosture.REGULAR, 16);
+        }
+
+        // combining all the predicted words into a list
+        if (i != 10) {
+          sb.append(i)
+              .append("  :  ")
+              .append(word.substring(0, 1).toUpperCase() + word.substring(1))
+              .append(System.lineSeparator());
+          text.setText(sb.toString());
+          text.setFont(font);
+          predictionsTextFlow.getChildren().add(text);
+        } else {
+          // special consideration for spacing if it is the 10th word
+          sb.append(i)
+              .append(" :  ")
+              .append(word.substring(0, 1).toUpperCase() + word.substring(1))
+              .append(System.lineSeparator());
+          text.setText(sb.toString());
+          text.setFont(font);
+          predictionsTextFlow.getChildren().add(text);
+        }
+        i++;
+      }
+
+    } else {
+      predictionsTitleLabel.setText("ROBO'S PREDICTIONS");
+      predictionsLabel.setText("Top 10 Predictions\n");
+    }
   }
 
   /** Enables user to erase what they have drawn. */

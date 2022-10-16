9a71075bc5739fbe83d3fa7cc514714a3e83e6cc
mkim897 <100652121+mkim897@users.noreply.github.com>
2022-10-16 16:37:50 +1300

    docs - Add comments and JavaDoc for MediaUtil class

--- src/main/java/nz/ac/auckland/se206/util/MediaUtil.java
+++ src/main/java/nz/ac/auckland/se206/util/MediaUtil.java
@@ -8,17 +8,24 @@ import nz.ac.auckland.se206.App;
 public class MediaUtil {
   private Media mediaFile;
   private MediaPlayer player;
+  // create strings for names of the sound files
   public static String buttonClickFile = "mixkit-typewriter-soft-click-1125.mp3";
   public static String winGameFile = "mixkit-fantasy-game-success-notification-270.mp3";
   public static String loseGameFile = "mixkit-retro-arcade-lose-2027.mp3";
   public static String fastTickingFile = "mixkit-fast-wall-clock-ticking-1063.mp3";
 
+  /**
+   * Constructor for the mediautil class
+   *
+   * @param fileName string input referencing the file name of the sound to be played
+   * @throws URISyntaxException throws error if string cannot be parsed as a URI reference
+   */
   public MediaUtil(String fileName) throws URISyntaxException {
     this.mediaFile = new Media(App.class.getResource("/sounds/" + fileName).toURI().toString());
     this.player = new MediaPlayer(mediaFile);
   }
 
-  /** Plays the mp3 file associated */
+  /** Plays the associated mp3 file */
   public void play() {
     player.play();
   }
